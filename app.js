4cd03eaf7a4b524ab42ea8c4f569436260a1d16b
Avishikta Bagchi <avi.bagchi04@gmail.com>
2026-03-26 09:33:20 -0500

    fix using azure PORT env variable

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(3000, () => {
-    console.log("App running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
 });
